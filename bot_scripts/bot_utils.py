ea51cdf42ab9ac4786d2390ecac1786ff2bde7d6
averelli <beloglazov.roman06@gmail.com>
2025-04-21 21:59:41 +0300

    chore: add utils functions for the bot

---
+++ bot_scripts/bot_utils.py
@@ -0,0 +1,34 @@
+import os
+import json
+import logging
+from telegram import Update
+from telegram.ext import ConversationHandler, ContextTypes
+from config.config import QUERIES_PATH
+
+logger = logging.getLogger("email-telegram-bot")
+
+def load_queries() -> list:
+    try:
+        with open(QUERIES_PATH, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        logger.info("Queries file does not exist, creating empty.")
+        with open(QUERIES_PATH, "w") as f:
+            json.dump([], f)
+            return []
+    except Exception as e:
+        logger.error(f"Error while loading queries: {e}", exc_info=True)
+        raise
+    
+def save_queries(queries:list):
+    try:
+        with open(QUERIES_PATH, "w") as f:
+            json.dump(queries, f, indent=2)
+    except Exception as e:
+        logger.error(f"Error while saving queries: {e}", exc_info=True)
+        raise
+
+async def cancel_fallback(upd: Update, ctx: ContextTypes.DEFAULT_TYPE):
+    await upd.message.reply_text("Operation cancelled")
+    logger.info("Operation cancelled with /cancel")
+    return ConversationHandler.END
