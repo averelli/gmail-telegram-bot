e65443803994fd052ef4d72805de64701b3a2c0d
averelli <beloglazov.roman06@gmail.com>
2025-04-25 11:23:16 +0300

    feat: add main script to run the app

---
+++ main.py
@@ -0,0 +1,15 @@
+from bot import build_bot_app
+from config import POLL_INTERVAL, setup_logging
+from gmail_poller import gmail_poller
+
+logger = setup_logging()
+
+def main():
+    logger.info("Starting the bot")
+    bot = build_bot_app()
+    bot.job_queue.run_repeating(callback=gmail_poller, interval=POLL_INTERVAL, first=0)
+    bot.run_polling()
+    logger.info("Bot stopped")
+
+if __name__ == "__main__":
+    main()
